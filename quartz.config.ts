84f31379bb9e0b035cdb61be80b0cde5f8619dab
arkarsg <alxarkar@gmail.com>
2023-11-23 03:53:21 +0800

    Update styles

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Some notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
           light: "#faf8f8",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
+          darkgray: "#161616",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
